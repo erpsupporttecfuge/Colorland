f9937781a5aeb5ea43ac7cdf6059b945c4e0b6cc
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2020-12-03 15:42:57 +0400

    stock dashboad update

---
+++ stock_quant_today_filter/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': " Stock Quant Today Filter",
+    'version': '1.1.1',
+    'category': 'Operations/Inventory',
+    'summary': """
+               """,
+    'description': """
+       
+    """,
+    "license": "Other proprietary",
+    'author': "Sananaz Mansuri",
+    'website': "www.odoo.com",
+    'depends': [
+                'stock'
+               ],
+    'data':[
+        'views/stock_quant_view.xml',
+    ],
+    'installable' : True,
+    'application' : False,
+    'auto_install' : False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
