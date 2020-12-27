c2800648256b0a86929672b63bb4a4cc83042c32
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2020-12-27 13:22:51 +0400

    pos_sa05

--- pos_salesagent/models/hr_employee.py
+++ pos_salesagent/models/hr_employee.py
@@ -4,6 +4,7 @@ class EmployeeMasterExtend(models.Model):
     _inherit = 'hr.employee'
 
     is_a_agent = fields.Boolean(string='Is a Sales Agent',
-                                 help='Enable this field to mark the normal employee as a Sales Agent')
+                                 help='Enable this field to mark the normal employee as a Sales Agent',
+                                groups = "hr.group_hr_user")
 
-    sales_commision = fields.Float('Sales Commision (%)', default=0.00)
+    sales_commision = fields.Float('Sales Commision (%)', default=0.00,  groups="hr.group_hr_user")
