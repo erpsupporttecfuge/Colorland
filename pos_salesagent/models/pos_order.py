4b465c1e2e76263b512f0f5c9176b805f5df51ec
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-01-05 00:06:40 +0400

    pos_sa

--- pos_salesagent/models/pos_order.py
+++ pos_salesagent/models/pos_order.py
@@ -7,6 +7,14 @@ class OrderLineCommission(models.Model):
 
     agent_comm_amt = fields.Float('Sales Agent Commision', default=0.00)
 
+    @api.model
+    def _order_line_fields(self, line, session_id=None):
+        orderline = super(OrderLineCommission, self)._order_line_fields(line)
+        product = self.env['product.product'].browse(orderline[2]['product_id'])
+        orderline[2]['agent_comm_amt'] = product.sales_agent_commision
+        return orderline
+
+
 
 class OrderNotes(models.Model):
     _inherit = 'pos.order'
@@ -16,37 +24,21 @@ class OrderNotes(models.Model):
 
     @api.model
     def _order_fields(self, ui_order):
-        order = super(OrderNotes, self)._order_fields(ui_order)
-        process_line = partial(self.env['pos.order.line']._order_line_fields, session_id=ui_order['pos_session_id'])
+        fields = super(OrderNotes, self)._order_fields(ui_order)
+        # process_line = partial(self.env['pos.order.line']._order_line_fields, session_id=ui_order['pos_session_id'])
         objemp = self.env['hr.employee'].search([('id', '=', ui_order['agent_id']  if "agent_id" in ui_order else False)], limit=1)
         agentcom=0.00
         if objemp:
             agentcom=objemp.sales_commision
-        updated_lines = ui_order['lines']
+        # updated_lines = ui_order['lines']
+        #
+        # for rec in updated_lines:
+        #     objproduct = self.env['product.product'].search([('id','=', rec[2]["product_id"])], limit=1)
+        #     if objproduct:
+        #         rec[2]["agent_comm_amt"]= objproduct.sales_agent_commision
 
-        for rec in updated_lines:
-            objproduct = self.env['product.product'].search([('id','=', rec[2]["product_id"])], limit=1)
-            if objproduct:
-                rec[2]["agent_comm_amt"]= objproduct.sales_agent_commision
+        fields['agent_id'] = ui_order.get('agent_id', 0)#ui_order['agent_id']  if "agent_id" in ui_order else False,
+        fields['agent_commision'] = float(agentcom or 0.0)
 
+        return fields
 
-        return {
-            'user_id':      ui_order['user_id'] or False,
-            'employee_id': ui_order['employee_id'] or False,
-            'session_id':   ui_order['pos_session_id'],
-            'lines':        [process_line(l) for l in ui_order['lines']] if ui_order['lines'] else False,
-            'pos_reference': ui_order['name'],
-            'sequence_number': ui_order['sequence_number'],
-            'partner_id':   ui_order['partner_id'] or False,
-            'date_order':   ui_order['creation_date'].replace('T', ' ')[:19],
-            'fiscal_position_id': ui_order['fiscal_position_id'],
-            'pricelist_id': ui_order['pricelist_id'],
-            'amount_paid':  ui_order['amount_paid'],
-            'amount_total':  ui_order['amount_total'],
-            'amount_tax':  ui_order['amount_tax'],
-            'amount_return':  ui_order['amount_return'],
-            'company_id': self.env['pos.session'].browse(ui_order['pos_session_id']).company_id.id,
-            'to_invoice': ui_order['to_invoice'] if "to_invoice" in ui_order else False,
-            'agent_id': ui_order['agent_id']  if "agent_id" in ui_order else False,
-            'agent_commision': agentcom,
-        }
