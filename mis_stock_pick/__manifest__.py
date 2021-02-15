8bb2165eb249a92961481dbb2ec3b06c65b7bc88
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-02-15 13:55:29 +0400

    update

---
+++ mis_stock_pick/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "MIS Stock Picking Customization for Barcode print",
+
+    'summary': """MIS Stock Picking Customization for Barcode print""",
+
+    'author': "Mindinfosys",
+    'website': "http://www.mindinfosys.com",
+
+    'category': 'Warehouse',
+    'version': '13.0.0.0.1',
+    'license': 'OPL-1',
+    'depends': ['stock','sale_stock'],
+
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+
+}
