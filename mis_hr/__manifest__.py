a657cebf20d72d5b95c44af091f5ea18aedaf1b2
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-06-14 18:08:36 +0400

    update

---
+++ mis_hr/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': 'MIS-HR Customization',
+    'version': '13.0.0.1',
+    'summary': """HR  Modification""",
+    'category': 'Employee',
+    'author': 'Hafeel Salim, hafeel.salim@mindinfosys.com',
+    'company': 'mindinfosys.com, UAE',
+    'description': 'HR  Customization',
+    'website': 'www.mindinfosys.com',
+    'depends': ['hr',],
+    'data': [
+        'views/mis_hr_employee.xml',
+    ],
+    'demo': [],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ mis_hr/models/mis_hr_employee.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+class MisAuhPartner1(models.Model):
+    _inherit = 'hr.employee'
+
+    joining_date = fields.Date('Joining Date', groups="hr.group_hr_user", tracking=True)
+    passport_issued_date = fields.Date('Passport Issued Date', groups="hr.group_hr_user", tracking=True)
+    passport_expiry_date = fields.Date('Passport Expiry Date', groups="hr.group_hr_user", tracking=True)
+    visa_issued_date = fields.Date('Visa Issued Date', groups="hr.group_hr_user", tracking=True)
+
+    work_issued_date = fields.Date('Work Permit Issued Date', groups="hr.group_hr_user", tracking=True)
+    work_expiry_date = fields.Date('Work Permit Expiry Date', groups="hr.group_hr_user", tracking=True)
+    insurance_no = fields.Char('Insurance No', groups="hr.group_hr_user", tracking=True)
+    insurance_issued_date = fields.Date('Insurance Issued Date', groups="hr.group_hr_user", tracking=True)
+    insurance_expiry_date = fields.Date('Insurance Expiry Date', groups="hr.group_hr_user", tracking=True)
+
+
