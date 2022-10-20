1268abc8f53971f70e5ecac340e9f77a1c6d3e3c
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2022-10-20 00:26:28 -0400

    Tax Invoice receipt in POS

---
+++ custom_po_tax_invoice_reciept/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    'name': 'POS TAX Invoice Reciept ',
+    'version': '1.0',
+    'category': 'Point Of Sale',
+    'summary': 'Customized Receipt of Point Of Sales',
+  'depends': ['base', 'point_of_sale'],
+    "data": [],
+    'demo': [],
+    'qweb': ['static/src/xml/pos.xml'],
+    'installable': True,
+}
